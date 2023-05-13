6be756ef1684f37623d53f6e96e1806f68b8fe40
sca-genki-kanno0005 <21cge0002is@edu.sca.ac.jp>
2023-05-13 16:08:08 +0900

    シーン切り替え

---
+++ Assets/gotofl/scrips/MenuPanel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPanel : MonoBehaviour
+{
+    [SerializeField] GameObject Panel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            var isActive = Panel.activeInHierarchy; // panelがアクティブか取得
+            Panel.SetActive(!isActive);             // 反転させる
+        }
+    }
+}
