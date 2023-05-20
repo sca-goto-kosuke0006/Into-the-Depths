a79168120d4ef519508cf6b1ecad5f7dbbca02be
yuki <sdt22csc0018@edu.sca.ac.jp>
2023-05-20 16:41:06 +0900

    ギミック移動

---
+++ Assets/hirano/script/Toge.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toge : MonoBehaviour
+{
+    [SerializeField]private float _length=1;
+   
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var amplitude = 2; // 振幅
+        var period = 1; // 一往復する周期（秒）
+        var t = 4 * amplitude * Time.time / period; // 時間の進行速度を調整
+        var value=Mathf.PingPong(t,2*amplitude)-amplitude;
+       
+
+        transform.localPosition = new Vector3(0, value, 0);
+    }
+}
