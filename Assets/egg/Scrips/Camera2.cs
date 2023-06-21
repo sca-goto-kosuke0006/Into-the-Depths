d588b19754d13476907829584d703c7b31ff13a8
syuma_ito <sdt22cge0001@edu.sca.ac.jp>
2023-06-21 12:38:53 +0900

    playerノーション

---
+++ Assets/egg/Scrips/Camera2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera2 : MonoBehaviour
+{
+    GameObject playerObj;
+    Player player;
+    Transform playerTransform;
+
+    void Start()
+    {
+        playerObj = GameObject.FindGameObjectWithTag("Player");
+        player = playerObj.GetComponent<Player>();
+        playerTransform = playerObj.transform;
+    }
+
+    void Update()
+    {
+        MoveCamera();
+    }
+
+    void MoveCamera()
+    {
+
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, transform.position.z);
+    }
+}
