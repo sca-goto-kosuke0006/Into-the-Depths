b9616b8e2d227bf65f6f78b272bbde76a6e37a6b
goto <>
2023-07-01 17:54:54 +0900

    新背景、playerのコントローラー、隠しステージへのシーン移動

---
+++ Assets/gotofl/scrips/PanelScrips/menu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // UIコンポーネントの使用
+
+public class menu : MonoBehaviour
+{
+	Button Button1;
+	Button Button2;
+	Button Button3;
+
+	void Start()
+	{
+		// ボタンコンポーネントの取得
+		Button1 = GameObject.Find("ruleButton").GetComponent<Button>();
+		Button2 = GameObject.Find("BackButton").GetComponent<Button>();
+		Button3 = GameObject.Find("TitelBackButton").GetComponent<Button>();
+
+		// 最初に選択状態にしたいボタンの設定
+		Button1.Select();
+	}
+}
