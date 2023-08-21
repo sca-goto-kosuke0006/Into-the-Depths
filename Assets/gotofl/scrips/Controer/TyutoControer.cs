67eb709fac5e3249c6221a45378525d5de54a890
goto <>
2023-08-21 12:31:31 +0900

    たいとるこんとろーらー

---
+++ Assets/gotofl/scrips/Controer/TyutoControer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TyutoControer : MonoBehaviour
+{
+    [SerializeField] GameObject Panel;
+    [SerializeField] GameObject Panel2;
+    [SerializeField] GameObject Panel3;
+    [SerializeField] Button StartButton;
+    [SerializeField] Button SkippuButton;
+    [SerializeField] Button resumeButton;
+
+    bool isPause = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
