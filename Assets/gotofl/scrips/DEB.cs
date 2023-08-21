0d5e5b491892687f189a19f834d392c669b1e012
goto <>
2023-08-21 10:32:45 +0900

    あたりはんてい

---
+++ Assets/gotofl/scrips/DEB.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DEB : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+     
+    }
+/*    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.constraints = RigidbodyConstraints2D.None;
+    }*/
+}
