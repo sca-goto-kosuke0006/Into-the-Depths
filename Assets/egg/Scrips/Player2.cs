42de5657e54410a6c62034dcb5fbe3a41386a744
goto <>
2023-08-26 09:56:11 +0900

    Playerの向き変更,Inputmanager削除

--- Assets/egg/Scrips/Player2.cs
+++ Assets/egg/Scrips/Player2.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class Player2 : MonoBehaviour
 {
     [SerializeField] float speed;
-
+    [SerializeField] SpriteRenderer sr;
     void Start()
     {
-
+        sr = this.GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         float x = Input.GetAxisRaw("Horizontal");
+        if (x < 0) sr.flipX = true;
+        else sr.flipX = false;
         Vector3 scale = transform.localScale;
-       
+
         // 代入し直す
         transform.localScale = scale;
         float y = Input.GetAxisRaw("Vertical");
@@ -24,8 +26,8 @@ public class Player2 : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.S))
         {
-            Destroy(gameObject);
-          
+            //Destroy(gameObject);
+
         }
 
     }
