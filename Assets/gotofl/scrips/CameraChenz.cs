2a6de665f1e672fde9ab170520767caffa6de769
goto <>
2023-08-31 16:32:37 +0900

    ステージ外への移動試し

---
+++ Assets/gotofl/scrips/CameraChenz.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChenz : MonoBehaviour
+{
+    GameObject mainCamObj;
+
+    private float cameraX = 0.0f;
+
+    private bool Flag;
+   
+    // Use this for initialization
+    void Start()
+    {
+        mainCamObj = Camera.main.gameObject;
+
+
+        Flag =false;
+    }
+
+    private void Update()
+    {
+        if (Flag == false)
+        {
+            void OnTriggerEnter2D(Collider2D collision)
+            {
+                
+                Debug.Log("入る");
+                Flag=true;
+            }
+        }
+        else
+        {
+            void OnTriggerEnter2D(Collider2D collision)
+            {
+               
+                Debug.Log("戻る");
+                Flag=false;
+            }
+        }
+    }
+
+}
