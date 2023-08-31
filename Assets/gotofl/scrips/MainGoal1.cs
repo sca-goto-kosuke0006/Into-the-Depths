a120cdfd12a755cb6606e594bb2d1cebf30953f8
goto <>
2023-09-01 00:09:30 +0900

    1920*1080

--- Assets/gotofl/scrips/MainGoal1.cs
+++ Assets/gotofl/scrips/MainGoal1.cs
@@ -5,20 +5,39 @@ using UnityEngine;
 public class MainGoal1 : MonoBehaviour
 {
     [SerializeField] GameObject Panel;
+    [SerializeField] GameObject PanelGoal;
     private bool _isMain1Goal;
 
+    private void Start()
+    {
+        _isMain1Goal = true;
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (!_isMain1Goal)
+        if (_isMain1Goal==true)
         {
             if (other.gameObject.tag == "Player")
             {
                 Panel.SetActive(true);
                 Time.timeScale = 0;
                 Debug.Log("ゴールした");
-                _isMain1Goal = true;
+                
             }
         }
+    }
+    public void OnClick1()
+    {
+        PanelGoal.SetActive(true);
+        Panel.SetActive(false);
+        _isMain1Goal = false;
+
+    }
+    public void OnClick2()
+    {
+        Panel.SetActive(false);
+
+        Time.timeScale = 1;
+
     }
 
 }
