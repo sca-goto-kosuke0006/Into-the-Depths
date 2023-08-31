e14f7d21f2ab1e1957f51c1f107dc2100f7fed7e
goto <>
2023-08-31 15:35:40 +0900

    spineアセット追加,画面比率調整,説明文追加

---
+++ Assets/gotofl/scrips/PanelScrips/StartsetumeiPanel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class StartsetumeiPanel : MonoBehaviour
+{
+    [SerializeField] GameObject StartPanel;
+    [SerializeField] GameObject Panel2;
+
+    private bool Flag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Flag =false;
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Flag == false)
+        {
+            if (Input.GetKeyDown("joystick button 0")||Input.GetKeyDown(KeyCode.Space)){
+            StartPanel.SetActive(false);
+            Panel2.SetActive(true);
+                Flag = true; } 
+        }
+
+       else
+        {
+            if (Input.GetKeyDown("joystick button 0") || Input.GetKeyDown(KeyCode.Space))
+            {
+             
+                Panel2.SetActive(false);
+                Time.timeScale = 1;
+            }
+        }
+    }
+    }
